97abbe659a28b739ad54468f5776e953722c3938
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-03-04 23:24:01 +0800

    add incompleted 31

---
+++ 31.next-permutation.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=31 lang=java
+ *
+ * [31] Next Permutation
+ *
+ * https://leetcode.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (30.05%)
+ * Total Accepted:    214.7K
+ * Total Submissions: 714.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Implement next permutation, which rearranges numbers into the
+ * lexicographically next greater permutation of numbers.
+ * 
+ * If such arrangement is not possible, it must rearrange it as the lowest
+ * possible order (ie, sorted in ascending order).
+ * 
+ * The replacement must be in-place and use only constant extra memory.
+ * 
+ * Here are some examples. Inputs are in the left-hand column and its
+ * corresponding outputs are in the right-hand column.
+ * 
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+
+            if(i == 0) {
+                int left = 0;
+                int right = nums.length - 1;
+                while(left < right) {
+                    nums[left] = nums[left] ^ nums[right];
+                    nums[right] = nums[right] ^ nums[left];
+                    nums[left] = nums[left] ^ nums[right];
+                    left++;
+                    right--;
+                }
+                return;
+            }
+
+            if(nums[i] > nums[i-1]) {
+                nums[i] = nums[i] ^ nums[i-1];
+                nums[i-1] = nums[i-1] ^ nums[i];
+                nums[i] = nums[i] ^ nums[i-1];
+                return;
+            }
+        }
+    }
+
+    // Run with 
+    public static void main(String[] args) {
+        // Test case -> Expected result map
+        Map<int[], int[]> testAndResultMap = new LinkedHashMap<>();
+
+        // Base Case
+        testAndResultMap.put(new int[]{1, 1, 1},  new int[]{1, 1, 1});
+
+        // Sorting order cases
+        // TODO: these tests are failing
+        testAndResultMap.put(new int[]{1, 2, 3},  new int[]{1, 3, 2});
+        testAndResultMap.put(new int[]{1, 3, 2},  new int[]{2, 1, 3});
+        testAndResultMap.put(new int[]{1, 8, 2, 7, 3},  new int[]{1, 8, 3, 2, 7});
+        testAndResultMap.put(new int[]{1, 2, 8, 7, 3},  new int[]{1, 3, 2, 7, 8});
+
+        // Reseting order cases
+        testAndResultMap.put(new int[]{3, 2, 1},  new int[]{1, 2, 3});
+        testAndResultMap.put(new int[]{5, 3, 2, 1},  new int[]{1, 2, 3, 5});
+
+        // Duplicated number cases
+        testAndResultMap.put(new int[]{1, 1, 5},  new int[]{1, 5, 1});
+        testAndResultMap.put(new int[]{1, 1, 5, 5, 5},  new int[]{1, 5, 1, 5, 5});
+
+        Solution test = new Solution();
+        testAndResultMap.entrySet().stream()
+            .forEach(entry -> {
+                try {
+                    test.nextPermutation(entry.getKey());
+                    boolean result = Arrays.equals(entry.getKey(), entry.getValue());
+                    System.out.println(result);
+                    if (!result) {
+                        System.out.println("Expected:");
+                        printArr(entry.getValue());
+                        System.out.println("Result:");
+                        printArr(entry.getKey());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+    }
+
+    private static void printArr(int[] arr) {
+        for (int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("");
+    }
+}
+
