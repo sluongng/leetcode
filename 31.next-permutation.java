f7547b9b3fc3b02588e95462237b96cf4a45463f
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-03-05 00:07:01 +0800

    Added comments

--- 31.next-permutation.java
+++ 31.next-permutation.java
@@ -53,16 +53,33 @@ class Solution {
 
             if(nums[i] > nums[i-1]) {
                 if (uglyCase) {
+
+                    // Traverse form right to left
+                    // all elements are in increasing orders
+                    // nums[i-1] is the first drop in value
+
+                    // Here we take nums[i-1] and swap it
+                    // with the minimum value (n) in nums[i -> nums.length]
+                    // such that n > nums[i-1]
+
+                    // accomplish that by traversing i-> end
+                    // taking the differences(distant) between current value and nums[i-1]
+                    // discard all distants less than 0
+                    // collect the min distant and min index
+
+                    // nums[i] > nums[i-1] so the default index here is `i`
+
                     int closestBiggerIndex = i;
                     for (int j = i; j < nums.length; j++) {
+
                         int minDistant = nums[closestBiggerIndex] - nums[i-1];
                         int currentDistant = nums[j] - nums[i-1];
+
                         if ( currentDistant > 0 && currentDistant < minDistant) {
                             minDistant = currentDistant;
                             closestBiggerIndex = j;
                         }
                     }
-
                     swap2Index(nums, closestBiggerIndex, i-1);
 
                     // sort nums[i -> nums.length - 1] ascending
@@ -80,9 +97,9 @@ class Solution {
     }
 
     private void swap2Index(int[] arr, int idx1, int idx2) {
-        arr[idx1]   = arr[idx1]   ^ arr[idx2];
+        arr[idx1] = arr[idx1] ^ arr[idx2];
         arr[idx2] = arr[idx2] ^ arr[idx1];
-        arr[idx1]   = arr[idx1]   ^ arr[idx2];
+        arr[idx1] = arr[idx1] ^ arr[idx2];
     }
 
     // Run with javac 31.next-permutation.java && java -cp . Solution
@@ -101,6 +118,7 @@ class Solution {
         testAndResultMap.put(new int[]{1, 2, 8, 7, 3, 3},  new int[]{1, 3, 2, 3, 7, 8});
         testAndResultMap.put(new int[]{1, 2, 8, 7, 3, 4},  new int[]{1, 2, 8, 7, 4, 3});
 
+        // Some more case with peak dropping(from right to left) at 0 index
         testAndResultMap.put(new int[]{2, 3, 1},  new int[]{3, 1, 2});
         testAndResultMap.put(new int[]{3, 9, 8, 2, 1},  new int[]{8, 1, 2, 3, 9});
 
