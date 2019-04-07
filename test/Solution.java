44c377b034d47d1f93bd621e1eff35ae45da0882
Son Luong Ngoc <son.luongngoc@lazada.com>
2019-04-07 15:05:22 +0800

    Something wrong still

--- test/Solution.java
+++ test/Solution.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -63,24 +61,70 @@ class Solution {
             // Traverse down the tree according to characters of current word
             for (char c : word.toCharArray()) {
                 int index = c - 'a';
-                if (currentTrie.getChild()[index] == null) {
-                    currentTrie.getChild()[index] = new Trie();
+                if (currentTrie.getNodes()[index] == null) {
+                    currentTrie.getNodes()[index] = new Trie();
                 }
-                currentTrie = currentTrie.getChild()[index];
+                currentTrie = currentTrie.getNodes()[index];
             }
 
             // At end of traversal, assign the word to child node
             currentTrie.setWord(word);
         }
 
+        List<String> result = new ArrayList<>();
         // Iterate over all elements of board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                int childIdx = board[i][j] - 'a';
+                dfs(board, i, j, myDict, result);
             }
         }
 
-        return Arrays.asList("Hello", "World");
+        return result;
+    }
+
+    /**
+     * <Depth first search solution from leetcode>
+     * we scan the board from 1 corner to the opposite corner[(0,0) -> (maxHeight, maxWidth)]
+     * Duplication:
+     * each time we scan, we temporarily replace content of current cell with a mark character 
+     * which signify that it has been scanned to avoid circular navigation 
+     * 
+     * @param board
+     * @param i
+     * @param j
+     * @param node
+     * @param result
+     */
+    public void dfs(char[][] board, int i, int j, Trie dict, List<String> result) {
+        char currChar = board[i][j];
+        int currIdx = currChar - 'a';
+
+        // Terminate if circular navigation on board
+        // OR that there is no word found in dictionary Trie
+        if (currChar == '#' || dict.getNodes()[currIdx] == null) {
+            return;
+        }
+        Trie currNode = dict.getNodes()[currIdx];
+
+        if (currNode.getWord() != null && !currNode.getWord().isEmpty()) {
+            result.add(currNode.getWord());
+
+            // we remove word out of dict 
+            // tree to avoid it being matched again
+            // This could also be avoid if we collect results using a Set instead of a List
+            currNode.setWord(null);
+        }
+
+        // Mark cell on board to avoid circular navigation
+        board[i][j] = '#';
+
+        if (i > 0) dfs(board, i - 1, j, dict, result);
+        if (j > 0) dfs(board, i, j - 1, dict, result);
+        if (i < board.length - 1) dfs(board, i + 1, j, dict, result);
+        if (j < board[i].length - 1) dfs(board, i, j + 1, dict, result);
+
+        // Restore the marking
+        board[i][j] = currChar;
     }
 
     public static void main(String[] args) {
@@ -123,24 +167,17 @@ class Solution {
 class Trie {
     // 26 character in the alphabets
     // index is calculated by `int index = currChar - 'a'`
-    private Trie[] child = new Trie[26];
+    private Trie[] nodes = new Trie[26];
     private String word = "";
 
     public Trie() {
     }
 
     /**
-     * @return the child
+     * @return the nodes
      */
-    public Trie[] getChild() {
-        return child;
-    }
-
-    /**
-     * @param child the child to set
-     */
-    public void setChild(Trie[] child) {
-        this.child = child;
+    public Trie[] getNodes() {
+        return nodes;
     }
 
     /**
